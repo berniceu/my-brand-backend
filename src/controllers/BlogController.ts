ae7c58b2103647bb0ebf784ecd3d9e8b43b9a435
bernice uwituze <b.uwituze@alustudent.com>
2024-04-19 21:13:22 +0200

    create blog controller

--- src/controllers/BlogController.ts
+++ src/controllers/BlogController.ts
@@ -2,7 +2,10 @@ import { Request, Response } from 'express';
 import BlogModel from '../models/BlogModel';
 import CommentModel from '../models/CommentModel';
 import dotenv from 'dotenv';
-import fs from 'fs'
+import uploader from '../cloudinary/cloudinary';
+import upload from '../cloudinary/multer';
+import fs from 'fs';
+
 
 dotenv.config();
 
@@ -11,5 +14,22 @@ const newBlog = async(req: Request, res: Response) => {
         if(!req.file){
             return res.status(400).json( {error: "No file uploaded"});
         }
+        const result = await uploader(req.file, res);
+        const { blogTitle, blog, author} = req.body;
+        const blogData = new BlogModel({
+            blogTitle,
+            blog,
+            blogImage: result.secure_url,
+            author
+        })
+
+        const newBlog = await blogData.save();
+
+        fs.unlinkSync(req.file.path);
+        res.status(200).json(newBlog)
+
+    } catch(err){
+        console.log(err);
+        return res.status(400).json(err)
     }
 }
